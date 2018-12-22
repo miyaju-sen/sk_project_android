c05b764852c55b00ef2740af08687cc4c8518238
WIN10OHS60224\ohs60224 <kawahara.hal@gmail.com>
2018-12-23 00:20:11 +0900

    EditTextWatcher完成 →テスト動作：概要編集時に使用

--- PlotEditor/app/src/main/java/local/hal/st32/android/ploteditor/EditTextWatcher.java
+++ PlotEditor/app/src/main/java/local/hal/st32/android/ploteditor/EditTextWatcher.java
@@ -7,26 +7,41 @@ import android.widget.EditText;
 /**
  * 就職作品
  *
- * EditTextの入力を監視するクラス
+ * EditTextの入力状態をリアルタイムで監視するクラス
  *
  * @author ohs60224
  */
 public class EditTextWatcher implements TextWatcher {
-    private  static EditText _editText;
+    private String _str;
+    private EditText _editText;
 
-    public static void setEditText(EditText editText) {
-        _editText = editText;
+    public void setEditText(EditText editText) {
+        this._editText = editText;
+        this._editText.addTextChangedListener(this);
     }
 
+    //入力前
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
     }
 
+    //入力最中
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
     }
 
+    //入力後
     @Override
     public void afterTextChanged(Editable s) {
+        //テキスト変更後に変更されたテキストを取り出す
+        this._str = s.toString();
+    }
+
+    /**
+     * 変更されたテキストのゲッター
+     * @return 変更されたテキスト
+     */
+    public String getStr() {
+        return this._str;
     }
 }
