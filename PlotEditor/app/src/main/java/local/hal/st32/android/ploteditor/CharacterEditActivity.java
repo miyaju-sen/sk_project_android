1d52ef75b19c42d6fd98e12967a1d14970f64385
WIN10OHS60224\ohs60224 <kawahara.hal@gmail.com>
2018-12-24 13:51:11 +0900

    EditTextWatcherをCharacterEditActivityで使用、戻るボタン押下時の処理を追加

--- PlotEditor/app/src/main/java/local/hal/st32/android/ploteditor/CharacterEditActivity.java
+++ PlotEditor/app/src/main/java/local/hal/st32/android/ploteditor/CharacterEditActivity.java
@@ -1,6 +1,7 @@
 package local.hal.st32.android.ploteditor;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -55,6 +56,10 @@ public class CharacterEditActivity extends AppCompatActivity {
      * 遷移元のアクティビティ（デフォルト：一覧画面）
      */
     private String ACTIVITY = new NowActivity().getCharacterListActivity();
+    /**
+     * EditTextの入力状態をリアルタイムで監視するクラス
+     */
+    private EditTextWatcher _watcher = new EditTextWatcher();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,22 +71,22 @@ public class CharacterEditActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         //画面部品取得
-        _etName = findViewById(R.id.etName);
-        _etPhonetic = findViewById(R.id.etPhonetic);
-        _etAnotherName = findViewById(R.id.etAnotherName);
+        _watcher.setEditText( _etName = findViewById(R.id.etName) );
+        _watcher.setEditText( _etPhonetic = findViewById(R.id.etPhonetic) );
+        _watcher.setEditText( _etAnotherName = findViewById(R.id.etAnotherName) );
         _rgAge = findViewById(R.id.rgAge);
         _rgGender = findViewById(R.id.rgGender);
-        _etHeight = findViewById(R.id.etHeight);
-        _etWeight = findViewById(R.id.etWeight);
-        _etFirstPerson = findViewById(R.id.etFirstPerson);
-        _etSecondPerson = findViewById(R.id.etSecondPerson);
-        _etBelongs = findViewById(R.id.etBelongs);
-        _etSkill = findViewById(R.id.etSkill);
-        _etProfile = findViewById(R.id.etProfile);
-        _etLivedProcess = findViewById(R.id.etLivedProcess);
-        _etPersonality = findViewById(R.id.etPersonality);
-        _etAppearance = findViewById(R.id.etAppearance);
-        _etOther = findViewById(R.id.etOther);
+        _watcher.setEditText( _etHeight = findViewById(R.id.etHeight) );
+        _watcher.setEditText( _etWeight = findViewById(R.id.etWeight) );
+        _watcher.setEditText( _etFirstPerson = findViewById(R.id.etFirstPerson) );
+        _watcher.setEditText( _etSecondPerson = findViewById(R.id.etSecondPerson) );
+        _watcher.setEditText( _etBelongs = findViewById(R.id.etBelongs) );
+        _watcher.setEditText( _etSkill = findViewById(R.id.etSkill) );
+        _watcher.setEditText( _etProfile = findViewById(R.id.etProfile) );
+        _watcher.setEditText( _etLivedProcess = findViewById(R.id.etLivedProcess) );
+        _watcher.setEditText( _etPersonality = findViewById(R.id.etPersonality) );
+        _watcher.setEditText( _etAppearance = findViewById(R.id.etAppearance) );
+        _watcher.setEditText( _etOther = findViewById(R.id.etOther) );
 
         _intent = getIntent();
         _plot = _intent.getStringExtra("PLOTNo"); //作品No
@@ -133,8 +138,7 @@ public class CharacterEditActivity extends AppCompatActivity {
                 break;
             //戻るボタン
             case android.R.id.home:
-                //TODO:戻る処理
-//                onBackButtonClick();
+                onBackButtonClick();
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -144,6 +148,16 @@ public class CharacterEditActivity extends AppCompatActivity {
      * 戻るボタン押下時の処理
      */
     private void onBackButtonClick() {
-
+        //TODO:画像パス、ラジオボタンの変更はどう判断するか
+        Boolean textChangeFlag = _watcher.isTextChange();
+        //変更されていた場合
+        if(textChangeFlag) {
+            ReturnConfirmDialogCreate dialog = new ReturnConfirmDialogCreate();
+            FragmentManager manager = getSupportFragmentManager();
+            dialog.show(manager, "CharacterEditActivity");
+        }
+        else {
+            finish();
+        }
     }
 }
