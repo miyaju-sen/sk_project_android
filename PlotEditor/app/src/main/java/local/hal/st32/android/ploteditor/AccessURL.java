4c0332a4fc48304a46b83ece11404b20b801d988
WIN10OHS60224\ohs60224 <kawahara.hal@gmail.com>
2019-01-05 14:10:45 +0900

    アクセス先のURLを変更

--- PlotEditor/app/src/main/java/local/hal/st32/android/ploteditor/AccessURL.java
+++ PlotEditor/app/src/main/java/local/hal/st32/android/ploteditor/AccessURL.java
@@ -20,8 +20,8 @@ public class AccessURL {
      * コンストラクタ
      */
     public AccessURL() {
-//        this.ROOT_URL = "http://192.168.0.111:8080/PlotEditor";
-        this.ROOT_URL = "http://192.168.0.43:8080/PlotEditor";
+        this.ROOT_URL = "http://192.168.0.111:8080/PlotEditor";
+//        this.ROOT_URL = "http://192.168.0.43:8080/PlotEditor";
         this.PLOT_JSON = ROOT_URL + "/PlotJsonServlet";
         this.CHARACTER_JSON = ROOT_URL + "/CharacterJsonServlet";
         this.STAGE_JSON = ROOT_URL + "/StageJsonServlet";
