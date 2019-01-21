a2c3d8f4421cfe7c40ec24d32a4247cb64c8dcb1
WIN10OHS60224\ohs60224 <kawahara.hal@gmail.com>
2019-01-21 22:20:05 +0900

    動的にViewを追加する動作をテスト

--- PlotEditor/app/src/main/java/local/hal/st32/android/ploteditor/TestActivity.java
+++ PlotEditor/app/src/main/java/local/hal/st32/android/ploteditor/TestActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -49,6 +50,10 @@ public class TestActivity extends AppCompatActivity {
     private final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
     private LinearLayout mLayout;
     private Button mBtAdd;
+    private View[] mViews = null;
+    private int cnt;
+
+    private final String TEXT_VIEW = "textView";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,24 +63,56 @@ public class TestActivity extends AppCompatActivity {
         mLayout = findViewById(R.id.layout);
 
         mBtAdd = mLayout.findViewById(R.id.btAdd);
+        mViews = new View[]{mBtAdd};
+
         mBtAdd.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                cnt = mViews.length;
+
+                //Viewが存在していた場合
+                if(0 != cnt) {
+                    mLayout.removeView(mBtAdd);
+                    cnt = cnt - 1;
+                }
+
                 Button button1 = new Button(TestActivity.this);
                 button1.setText("Button1");
-                mLayout.addView(button1, 1, createParam(WC, WC));
+                mLayout.addView(button1, cnt, createParam(WC, WC));
+                cnt = cnt + 1;
 
-                TextView text = new TextView(TestActivity.this);
-                text.setText("Text");
-                mLayout.addView(text, createParam(WC, WC));
+                button1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //最後のViewを指定して削除（インデックスは0始まり）
+                        View childView = mLayout.getChildAt(cnt - 1);
+                            //指定したViewがテキストビューなら削除
+                            if(TEXT_VIEW.equals(childView.getTag())) {
+                                mLayout.removeViewAt(cnt - 1);
+                                cnt = cnt - 1;
+                            }
+
+                        TextView text2 = new TextView(TestActivity.this);
+                        text2.setText("Text2");
+                        mLayout.addView(text2, cnt, createParam(WC, WC));
+                        cnt = cnt + 1;
+                    }
+                });
+
 
                 Button button2 = new Button(TestActivity.this);
                 button2.setText("Button2");
-                mLayout.addView(button2, 2, createParam(WC, WC));
+                mLayout.addView(button2, cnt, createParam(WC, WC));
+                cnt = cnt + 1;
+
+                TextView text = new TextView(TestActivity.this);
+                text.setText("Text");
+                text.setTag(TEXT_VIEW);
+                mLayout.addView(text, cnt, createParam(WC, WC));
+                cnt = cnt + 1;
             }
         });
-
 
     }
 
