bfbebf68fd55e0490e2c7c0fd2876bbf9cbbc3e4
WIN10OHS60224\ohs60224 <kawahara.hal@gmail.com>
2019-01-22 21:21:04 +0900

    メモ内容編集後のデータ反映完了

--- PlotEditor/app/src/main/java/local/hal/st32/android/ploteditor/MemoActivity.java
+++ PlotEditor/app/src/main/java/local/hal/st32/android/ploteditor/MemoActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
